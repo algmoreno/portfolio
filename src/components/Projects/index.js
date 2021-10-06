cb98305d84634860015ad8ebefc0db73b1da9dfc
algmoreno <alg.moreno00@gmail.com>
2021-10-06 12:23:53 -0700

    project buttons and links

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,45 +9,34 @@ function Projects() {
       <h2>Portfolio</h2>
       <div className='col-4 projects-col'>
         <Card className='project-card pet-shop-card'>
-
+          <Button className='view-btn' target='blank' href='https://github.com/bobswiftfoot/pet-shop'>GitHub</Button>
+          <Button className='view-btn' target='blank' href='https://codefellas-pet-shop.herokuapp.com/'>View App</Button>
         </Card>
         <Card className='project-card codequiz-card'>
-          
+        <Button className='view-btn' target='blank' href='https://github.com/algmoreno/codequiz'>GitHub</Button>
+        <Button className='view-btn' target='blank' href='https://algmoreno.github.io/codequiz'>View App</Button>
         </Card>
       </div>
       
       <div className='col-4 projects-col'>
       <Card className='project-card congo-card'>
-          <Card.Body>
-            
-          </Card.Body>
+          <Button className='view-btn' target='blank' href='https://github.com/algmoreno/Project-2'>GitHub</Button>
+          <Button className='view-btn' href='https://congomedia.herokuapp.com'>View App</Button>
         </Card>
         <Card className='project-card blogspot-card'>
-          <Card.Body>
-            
-          </Card.Body>
+          <Button className='view-btn' target='blank' href='https://github.com/algmoreno/blogspot'>GitHub</Button>
+          <Button className='view-btn' target='blank' href='https://blogpostspot.herokuapp.com'>View App</Button>
         </Card>
       </div>
 
       <div className='col-4 projects-col'>
-      <Card className='project-card weatherview-card'>
-          <Card.Body>
-            <Card.Title></Card.Title>
-            <Card.Text>
-
-            </Card.Text>
-
-          </Card.Body>
+        <Card className='project-card weatherview-card'>
+          <Button className='view-btn' target='blank' href='https://github.com/algmoreno/weatherview'>GitHub</Button>
+          <Button className='view-btn' target='blank' href='https://algmoreno.github.io/weatherview'>View App</Button>
         </Card>
         <Card className='project-card jukebox-card'>
-
-          <Card.Body>
-            <Card.Title></Card.Title>
-            <Card.Text>
-
-            </Card.Text>
-
-          </Card.Body>
+          <Button className='view-btn' target='blank' href='https://github.com/algmoreno/arm'>GitHub</Button>
+          <Button className='view-btn' target='blank' href='https://algmoreno.github.io/arm/'>View App</Button>
         </Card>
       </div>
     </div>
